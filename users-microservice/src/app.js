50d3d24b8b80a63aae4fb687fa0df8a9d54bfac1
Jessica Simplicio <jessicasimplicio1@gmail.com>
2025-11-17 17:54:16 -0300

    feat: add initial files to create users service

---
+++ users-microservice/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+  console.log(`Users microservice running on port ${PORT}`)
+})
+
+module.exports = app
