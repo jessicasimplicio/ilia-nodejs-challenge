6f74ef2e7f76fd7b4c7531067d3d1b3461ec94c2
Jessica Simplicio <jessicasimplicio1@gmail.com>
2025-11-17 20:05:36 -0300

    feat: add prettier

--- users-microservice/src/app.js
+++ users-microservice/src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
   console.log(`Users microservice running on port ${PORT}`)
 })
