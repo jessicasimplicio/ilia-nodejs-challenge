fcd2e1d3ca61e23b5968288d87da92d351af613a
Jessica Simplicio <jessicasimplicio1@gmail.com>
2025-11-17 14:25:16 -0300

    fix: place next() correctly

--- wallet-microservice/src/middleware/auth.js
+++ wallet-microservice/src/middleware/auth.js
@@ -6,16 +6,15 @@ const auth = (req, res, next) => {
     const token = req.headers.authorization
 
     if(!token) {
-        return res.status(401).json({ message: 'Not authorized' })  
+        return res.status(401).json({ message: 'Access token is missing or invalid' })  
     }
-    next()
 
     try{
         const decoded = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET)
+        next()
     } catch(err){
-        return res.status(401).json({ message: 'Invalid Token' })
+        return res.status(401).json({ message: 'Access token is missing or invalid' })
     }
-
 }
 
 module.exports = auth
