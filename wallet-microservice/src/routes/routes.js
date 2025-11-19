ba9950da29871317ed668ab90c8f1e2524c5fd32
Jessica Simplicio <jessicasimplicio1@gmail.com>
2025-11-19 00:35:18 -0300

    feat: remove health  checking route

--- wallet-microservice/src/routes/routes.js
+++ wallet-microservice/src/routes/routes.js
@@ -10,8 +10,5 @@ const router = express.Router()
 router.post('/transactions', createTransaction)
 router.get('/transactions', getTransactions)
 router.get('/balance/:user_id', getBalance)
-router.get('/health', (req, res) => {
-  res.status(200).json({ status: 'OK', service: 'Wallet Microservice' })
-})
 
 module.exports = router
