949edb337d049c057f705cc1465e5c2e5f48e538
Jessica Simplicio <jessicasimplicio1@gmail.com>
2025-11-25 18:49:52 -0300

    lint: remove console.log()

--- wallet-microservice/src/db/conn.js
+++ wallet-microservice/src/db/conn.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 
 const connectDb = async () => {
   const uri = process.env.DATABASE_URL
-  console.log('uri', uri)
   if (!uri) {
     throw new Error('Missing DATABASE_URL environment variable')
   }
